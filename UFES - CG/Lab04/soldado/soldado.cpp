66cd0976e5964218d1199f6b43a738e6f5575369
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-08-18 22:43:21 -0300

    updated OpenGL graphic computing code (incomplete)

--- UFES - CG/Lab04/soldado/soldado.cpp
+++ UFES - CG/Lab04/soldado/soldado.cpp
@@ -83,13 +83,31 @@ void ChangeCoordSys(
     GLdouble bx, GLdouble by, GLdouble bz,
     GLdouble upx, GLdouble upy, GLdouble upz)
 {
-    float x[3], y[3], z[3];
-    GLfloat m[4][4] = {1, 0, 0, 0,
-                       0, 1, 0, 0,
-                       0, 0, 1, 0,
-                       0, 0, 0, 1};
+    float dirVect[3] = {
+        float(ax - bx),
+        float(ay - by),
+        float(az - bz)};
+    normalize(dirVect);
+    float dirAngle = atan2(dirVect[1], dirVect[0]);
+    if (isnan(dirAngle))
+    {
+        dirAngle = 0.0;
+    }
 
-    // TODO: COLOQUE SEU CODIGO AQUI
+    cout << ax << " | " << bx << '\t' << ay << " | " << by << endl;
+    // cross();
+
+    float x[3], y[3], z[3];
+
+    // rotaciona ao redor de X
+    x[0] = 1.0 * upx, x[1] = 0.0 * upx, x[2] = 0.0 * upx;
+    y[0] = 0.0 * upy, y[1] = cos(dirAngle) * upy, y[2] = -sin(dirAngle) * upy;
+    z[0] = 0.0 * upz, z[1] = sin(dirAngle) * upz, z[2] = cos(dirAngle) * upz;
+
+    GLfloat m[4][4] = {x[0], x[1], x[2], 0,
+                       y[0], y[1], y[2], 0,
+                       z[0], z[1], z[2], 0,
+                       0, 0, 0, 1};
 
     glMultMatrixf(&m[0][0]);
 }
@@ -135,11 +153,11 @@ void DrawAxes(double size)
     glPopMatrix();
 }
 
-// TODO: ALTERE AQUI - SEU CODIGO AQUI
-// Usar meshlab para obter os pontos abaixo
-int pontoArmaAponta = 0;
-int pontoPosicaoArma = 0;
-int up[3] = {0, 0, 0};
+// obtidos via meshlab
+int pontoPosicaoArma = 4601;
+int pontoArmaAponta = 6905;
+int up[3] = {1, 1, -1};
+
 void desenhaJogador()
 {
     glMatrixMode(GL_MODELVIEW);
