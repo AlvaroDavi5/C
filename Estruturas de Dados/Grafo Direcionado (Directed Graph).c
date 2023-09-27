dad577da158b190630e191c2578453c2fedbbaae
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-09-26 21:49:08 -0300

    feat: printed and added DFS to directed graph

--- Estruturas de Dados/Grafo Direcionado (Directed Graph).c
+++ Estruturas de Dados/Grafo Direcionado (Directed Graph).c
@@ -1,48 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <assert.h>
 
-
-#define FALSE 0
-#define TRUE 1
-
+#define MAX_DISTANCE 999999
+#define isAValidWeight(w) (w > 0 && w < MAX_DISTANCE)
 
 struct graph
 {
-	int num_nodes;
-	bool **edges;
+	int verticesAmount;
+	int edgesAmount;
+	float **edges;
 };
 typedef struct graph Graph;
 
-
-Graph * createGraph(int num_nodes);
+Graph *createGraph(int verticesAmount);
 void destroyGraph(Graph *graph);
-bool addEdge(Graph *graph, unsigned int from_node, unsigned int to_node);
-bool hasEdge(Graph *graph, unsigned int from_node, unsigned int to_node);
-void displayGraph(Graph *graph);
-
+bool addEdge(Graph *graph, unsigned int src, unsigned int dest, float weight);
+bool hasEdge(Graph *graph, unsigned int src, unsigned int dest);
+void depthFirstSearch(Graph *graph, int vertex, bool *visited);
+void displayGraphEdges(Graph *graph);
+void displayGraphTable(Graph *graph);
 
 int main()
 {
-	Graph *g = createGraph(5);
+	Graph *graph = createGraph(5);
 
-	addEdge(g, 0, 1);
-	addEdge(g, 0, 2);
-	addEdge(g, 1, 3);
-	addEdge(g, 3, 4);
-	addEdge(g, 4, 1);
-	addEdge(g, 4, 0);
+	addEdge(graph, 0, 1, 1.0);
+	addEdge(graph, 0, 2, 2.0);
+	addEdge(graph, 1, 3, 2.3);
+	addEdge(graph, 2, 4, 2.0);
+	addEdge(graph, 3, 4, 1.2);
+	addEdge(graph, 4, 1, 3.1);
+	addEdge(graph, 4, 0, 4.5);
 
-	displayGraph(g);
+	displayGraphTable(graph);
+	displayGraphEdges(graph);
 
-	destroyGraph(g);
+	bool visitedVertices[graph->verticesAmount];
+	for (int i = 0; i < graph->verticesAmount; i++)
+		visitedVertices[i] = false;
+
+	depthFirstSearch(graph, 0, visitedVertices);
+
+	destroyGraph(graph);
 
 	return 0;
 }
 
-
-Graph * createGraph(int num_nodes)
+Graph *createGraph(int verticesAmount)
 {
 	Graph *gr = malloc(sizeof(Graph));
 	if (gr == NULL)
@@ -51,21 +56,31 @@ Graph * createGraph(int num_nodes)
 		return NULL;
 	}
 
-	gr->num_nodes = num_nodes;
-	gr->edges = calloc(sizeof(bool *), gr->num_nodes);
+	gr->verticesAmount = verticesAmount;
+	gr->edgesAmount = 0;
+
+	gr->edges = (float **)malloc(verticesAmount * sizeof(float *));
 	if (gr->edges == NULL)
 	{
 		free(gr);
 		return NULL;
 	}
 
-	for (int i = 0; i < gr->num_nodes; i++)
+	for (int i = 0; i < verticesAmount; i++)
 	{
-		(gr->edges[i]) = calloc(sizeof(bool), gr->num_nodes);
+		gr->edges[i] = (float *)malloc(verticesAmount * sizeof(float));
 		if (gr->edges[i] == NULL)
 		{
 			destroyGraph(gr);
 			return NULL;
+		}
+
+		for (int j = 0; j < verticesAmount; ++j)
+		{
+			if (i == j)
+				gr->edges[i][j] = 0.0;
+			else
+				gr->edges[i][j] = MAX_DISTANCE;
 		}
 	}
 
@@ -75,47 +90,67 @@ Graph * createGraph(int num_nodes)
 void destroyGraph(Graph *graph)
 {
 	if (graph->edges == NULL)
-	{
 		return;
-	}
 
-	for (int i = 0; i < graph->num_nodes; i++)
+	for (int i = 0; i < graph->verticesAmount; i++)
 	{
 		if (graph->edges[i] != NULL)
 		{
 			free(graph->edges[i]);
 		}
 	}
+
 	free(graph->edges);
+	free(graph);
 }
 
-bool addEdge(Graph *graph, unsigned int from_node, unsigned int to_node)
+bool addEdge(Graph *graph, unsigned int src, unsigned int dest, float weight)
 {
-	if (hasEdge(graph, from_node, to_node))
-		return FALSE;
+	if (hasEdge(graph, src, dest))
+		return false;
 
-	graph->edges[from_node][to_node] = true;
+	graph->edges[src][dest] = weight;
+	graph->edgesAmount++;
 
-	return TRUE;
+	return true;
 }
 
-bool hasEdge(Graph *graph, unsigned int from_node, unsigned int to_node)
+bool hasEdge(Graph *graph, unsigned int src, unsigned int dest)
 {
-	assert(graph != NULL);
-	assert(from_node < graph->num_nodes);
-	assert(to_node < graph->num_nodes);
+	if (graph == NULL)
+		return false;
+	else if ((src > graph->verticesAmount - 1) || (dest > graph->verticesAmount - 1))
+		return false;
 
-	return graph->edges[from_node][to_node];
+	if (graph->edges[src][dest] < MAX_DISTANCE)
+		return true;
+
+	return false;
 }
 
-void displayGraph(Graph *graph)
+void depthFirstSearch(Graph *graph, int vertex, bool *visited)
 {
-	printf("Digraph: {\n");
-	for (int from = 0; from < graph->num_nodes; from++)
+	visited[vertex] = true;
+	printf("Visited vertex: %d\n", vertex);
+
+	for (int i = 0; i < graph->verticesAmount; i++)
 	{
-		for (int to = 0; to < graph->num_nodes; to++)
+		if (!visited[i] && isAValidWeight(graph->edges[vertex][i]))
 		{
-			if ((graph->edges)[from][to])
+			depthFirstSearch(graph, i, visited);
+		}
+	}
+}
+
+void displayGraphEdges(Graph *graph)
+{
+	printf("Edges: {\n");
+	for (int from = 0; from < graph->verticesAmount; from++)
+	{
+		for (int to = 0; to < graph->verticesAmount; to++)
+		{
+			float w = graph->edges[from][to];
+			if (w > 0 && w < MAX_DISTANCE)
 			{
 				printf("%d -> %d;\n", from, to);
 			}
@@ -124,3 +159,21 @@ void displayGraph(Graph *graph)
 	printf("}\n");
 }
 
+void displayGraphTable(Graph *graph)
+{
+	printf("Table:\n");
+	for (int i = 0; i < graph->verticesAmount; i++)
+	{
+		for (int j = 0; j < graph->verticesAmount; j++)
+		{
+			float w = graph->edges[i][j];
+			if (w > 0 && w < MAX_DISTANCE)
+				printf("%.1f, ", w);
+			else if (w == 0)
+				printf(" * , ");
+			else
+				printf(" - , ");
+		}
+		printf("\n");
+	}
+}
